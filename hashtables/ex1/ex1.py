cba2b9141c0c51b821dfdb3b52a65fdcb89e2bae
BrandyBecker <brandybeckerfsw@gmail.com>
2020-07-10 13:17:41 -0400

    answers interview questions and examples 1, 2, and 4 for mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,17 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Your code here -------------------
+    data = {}
+    for i in range(len(weights)):
+        data[weights[i]] = i
+    for i in range(len(weights)):
+        dif = limit-weights[i]
+        if dif in data:
+            return (max(i, data[dif]), min(i, data[dif]))
+    # ----------------------------------
 
     return None
+
+#--------------------------
+# Ran 4 tests in 0.001s
